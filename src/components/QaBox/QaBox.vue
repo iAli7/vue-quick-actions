1a80f6b3661e193e644507b2edf9bb5c77c4c7fd
iAli7 <aligokyer206@gmail.com>
2023-07-11 02:26:21 +0300

    separate 'QaBoxSearch'

--- src/components/QaBox/QaBox.vue
+++ src/components/QaBox/QaBox.vue
@@ -1,14 +1,13 @@
 <template>
     <div class="quick-action">
-        <div class="quick-action-search">
-            <input placeholder="Search for apps and commands..." autofocus class="quick-action-search-input" type="text">
-        </div>
+        <QaBoxSearch />
         <QaBoxContent />
     </div>
 </template>
 
 <script setup lang="ts">
 import QaBoxContent from "../QaBoxContent/QaBoxContent.vue"
+import QaBoxSearch from "../QaBoxSearch/QaBoxSearch.vue"
 </script>
 
 <style src="./QaBox.scss" lang="scss" scoped/>
---
+++ src/components/QaBoxSearch/QaBoxSearch.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="quick-action-search">
+        <input placeholder="Search for apps and commands..." autofocus class="quick-action-search-input" type="text">
+    </div>
+</template>
+
+<style src="./QaBoxSearch.scss" lang="scss" />
