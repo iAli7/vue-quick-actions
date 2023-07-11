1c0268a04c733f404163f554d9954b12ce1ce9b0
iAli7 <aligokyer206@gmail.com>
2023-07-11 15:49:30 +0300

    fix search bug

--- src/components/QaBox/QaBox.vue
+++ src/components/QaBox/QaBox.vue
@@ -1,13 +1,61 @@
 <template>
     <div class="quick-action">
-        <QaBoxSearch />
-        <QaBoxContent />
+        <div class="quick-action-search">
+            <input placeholder="Search for apps and commands..." v-model="searchValue" autofocus
+                class="quick-action-search-input" type="text">
+        </div>
+        <div class="quick-action-content">
+            <div class="quick-action-content-title">Suggestions</div>
+            <div class="quick-action-list" v-for="(item, index) in QaList" :key="index">
+                <div class="quick-action-list-item" @click="item.onSelect()">
+                    <div class="quick-action-list-item-title">
+                        {{ item.name }}
+                    </div>
+                    <div class="quick-action-list-item-role">
+                        {{ item.role }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import QaBoxContent from "../QaBoxContent/QaBoxContent.vue"
-import QaBoxSearch from "../QaBoxSearch/QaBoxSearch.vue"
+import { ref, onMounted, watch } from 'vue';
+import { useQuickActions, shortcutItem } from '../../lib/utils/useQuickAction/useQuickAction';
+import { handleQaSearch } from '../../lib/utils/QaSearch';
+
+const searchValue = ref("")
+const QaList = ref<shortcutItem[]>([]);
+const variables: shortcutItem[] = [
+    {
+        name: 'Figma',
+        role: 'application',
+        onSelect: () => {
+            console.log('test');
+        },
+    },
+    {
+        name: 'YouTube',
+        role: 'application',
+        onSelect: () => {
+            console.log('test');
+        },
+    },
+];
+
+watch(searchValue, () =>{
+    QaList.value = handleQaSearch(searchValue)
+})
+
+onMounted(() => {
+    useQuickActions().setShortcutItems(variables);
+    QaList.value = useQuickActions().shortCutItems.items;
+});
+
 </script>
 
-<style src="./QaBox.scss" lang="scss" scoped/>
+<style src="./QaBox.scss" lang="scss"/>
+<style src="../QaBoxList/QaBoxList.scss" lang="scss"/>
+<style src="../QaBoxSearch/QaBoxSearch.scss" lang="scss"/>
+<style src="../QaBoxContent/QaBoxContent.scss" lang="scss"/>
