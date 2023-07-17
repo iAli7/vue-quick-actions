aefe5d293220385bb0222c242bfbc8686ce5925d
iAli7 <aligokyer206@gmail.com>
2023-07-17 03:28:19 +0300

    update files for npm

---
+++ .d.ts
@@ -0,0 +1,27 @@
+import { DefineComponent, Ref } from 'vue';
+import { shortcutItem } from './src/lib/types/types';
+
+export const QaBox: DefineComponent<
+  {
+    items: shortcutItem[];
+  },
+  {},
+  {
+    search: (value: string) => void;
+  },
+  {},
+  {
+    searchValue: Ref<string>;
+    focusItem: Ref<number>;
+    loading: Ref<boolean>;
+    showQuickAction: Ref<boolean>;
+    activeParentPath: Ref<shortcutItem[]>;
+    originalQaList: Ref<shortcutItem[]>;
+    qaList: Ref<shortcutItem[]>;
+    handleSelect: (item: shortcutItem) => void;
+    itemsToRender: Ref<shortcutItem[]>;
+    navigate: (direction: number) => number;
+    handleClickPath: (item: shortcutItem) => void;
+    handleKeyboard: (event: KeyboardEvent) => void;
+  }
+>;
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import path from 'path';
 
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
