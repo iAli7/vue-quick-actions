4185a6f89bf43dd4b43145115e726adeb0268c0d
iAli7 <aligokyer206@gmail.com>
2023-07-23 02:35:34 +0300

    remove footer keys

--- src/lib/components/QaFooter/QaFooter.vue
+++ src/lib/components/QaFooter/QaFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    v-if="keyboard || activeParentPath.length > 0"
+    v-if="activeParentPath.length > 0"
     class="quick-action-footer"
   >
     <div class="quick-action-badge">
@@ -13,49 +13,6 @@
         {{ item.label + (index < activeParentPath.length - 1 ? ' -> ' : '') }}
       </span>
     </div>
-    <div
-      v-if="keyboard"
-      class="quick-action-footer-keys"
-    >
-      <div class="quick-action-footer-keys-item">
-        ESC
-      </div>
-      <div class="quick-action-footer-keys-item">
-        Enter
-      </div>
-      <div class="quick-action-footer-keys">
-        <div class="quick-action-footer-keys-item">
-          <svg
-            width="6"
-            height="4"
-            viewBox="0 0 6 4"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            q:key="NT_0"
-          >
-            <path
-              d="M3 0L6 4H0L3 0Z"
-              fill="white"
-            />
-          </svg>
-        </div>
-        <div class="quick-action-footer-keys-item">
-          <svg
-            width="6"
-            height="4"
-            viewBox="0 0 6 4"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            q:key="5y_0"
-          >
-            <path
-              d="M3 4L0 0L6 5.24537e-07L3 4Z"
-              fill="white"
-            />
-          </svg>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -65,7 +22,6 @@ const props = defineProps<{
     type: Array,
     required: true,
   },
-  keyboard: Boolean
 }>();
 
 const emit = defineEmits<{
